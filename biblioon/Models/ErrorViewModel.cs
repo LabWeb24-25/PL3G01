5df6dd2414e8b95b9e096277e0693936be7986f0
Filipa Monteiro <filipa.monteiro330@gmail.com>
2024-11-22 10:22:44 +0000

    adiciona projeto

---
+++ biblioon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace biblioon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
