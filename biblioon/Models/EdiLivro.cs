4c7e96122df1b81f4063e177b437820acd8ed93a
João Esteves <joaoesteves10A4@sapo.pt>
2024-12-05 16:20:10 +0000

    inicia implementação das classes

---
+++ biblioon/Models/EdiLivro.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace biblioon.Models
+{
+    public class EdiLivro
+    {
+        [Key]
+        public required int Isbn { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required string Sinopse { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required string Capa { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required string [] Idioma { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required string DescFisica { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required DateOnly DataPublicacao { get; set; }
+
+    }
+
+}
---
+++ biblioon/Models/UniLivro.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace biblioon.Models
+{
+    public class UniLivro
+    {
+        [Key]
+        public required int Id { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required string Isbn { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required string Estado { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required float PrecoAquisicao { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required DateOnly DataAquisicao { get; set; }
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required bool Requisitado { get; set; } 
+
+        [Required(ErrorMessage = "Este é um field obrigatório.")]
+        public required bool Disponivel { get; set; }
+
+        public string? Anotacoes { get; set; }
+
+    }
+}
