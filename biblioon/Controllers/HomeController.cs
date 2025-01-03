e59a23c7af4d64f0ee88dcef242328fc4df950d6
Daydream127 <131152155+Daydream127@users.noreply.github.com>
2025-01-03 22:02:28 +0000

    fix links e criar paginas extra

--- biblioon/Controllers/HomeController.cs
+++ biblioon/Controllers/HomeController.cs
@@ -47,6 +47,11 @@ namespace biblioon.Controllers
             return View();
         }
 
+        public IActionResult Info()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
